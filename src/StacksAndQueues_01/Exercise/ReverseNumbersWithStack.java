cd28b3b922a1cb6ee407be300c1db823447604f9
Borislav Boychinov <borislavboychinov@gmail.com>
2023-05-11 15:09:53 +0300

    Java-Advanced2023

---
+++ src/StacksAndQueues_01/Exercise/ReverseNumbersWithStack.java
@@ -0,0 +1,11 @@
+package StacksAndQueues_01.Exercise;
+
+import java.util.Scanner;
+
+public class ReverseNumbersWithStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
