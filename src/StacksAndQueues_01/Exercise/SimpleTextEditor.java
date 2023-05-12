e504a089261143f244b61ea65ef790d67555e754
Borislav Boychinov <borislavboychinov@gmail.com>
2023-05-12 14:41:49 +0300

    Java-Advanced2023

--- src/StacksAndQueues_01/Exercise/SimpleTextEditor.java
+++ src/StacksAndQueues_01/Exercise/SimpleTextEditor.java
@@ -10,23 +10,22 @@ public class SimpleTextEditor {
         int n = Integer.parseInt(sc.nextLine());
 
         ArrayDeque<String> history = new ArrayDeque<>();
-        history.push("");
         StringBuilder text = new StringBuilder();
 
-        String commands = sc.nextLine();
         for (int i = 1; i <= n; i++) {
+           String commands = sc.nextLine();
             String[] tokens = commands.split("\\s+");
 
             switch (tokens[0]) {
                 case "1":
-                    history.push(tokens[1]);
+                    history.push(text.toString());
                     text.append(tokens[1]);
                     break;
                 case "2":
                     int count = Integer.parseInt(tokens[1]);
-                    for (int j = 0; j < count; j++) {
-                        text.deleteCharAt(text.length() - 1);
-                    }
+                    history.push(text.toString());
+                    int startIndex = text.length() - count;
+                    text.delete(startIndex, text.length());
                     break;
                 case "3":
                     System.out.println(text.charAt(Integer.parseInt(tokens[1]) - 1));
@@ -37,7 +36,6 @@ public class SimpleTextEditor {
                     }
                     break;
             }
-            commands = sc.nextLine();
         }
     }
 }
