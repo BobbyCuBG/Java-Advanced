1c07c93786f6d528abbdba72f5f748b7555cbf15
Borislav Boychinov <borislavboychinov@gmail.com>
2023-05-21 16:54:04 +0300

    Java-Advanced2023

--- src/_04_StreamsFilesDirectories/Lab/_08_NestedFolders.java
+++ src/_04_StreamsFilesDirectories/Lab/_08_NestedFolders.java
@@ -2,27 +2,29 @@ package _04_StreamsFilesDirectories.Lab;
 
 import java.io.File;
 import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class _08_NestedFolders {
     public static void main(String[] args) {
         String path = "/Users/cubg/IdeaProjects/JavaAdvanced2023/src/_04_StreamsFilesDirectories/Lab/04. Java-Advanced-Files-and-Streams-Lab-Resources/Files-and-Streams";
         File root = new File(path);
-        Deque<File> dirs = new ArrayDeque<>();
+
+        ArrayDeque<File> dirs = new ArrayDeque<>();
+
         dirs.offer(root);
 
         int count = 0;
+        //Breadth-First Search (BFS)
         while (!dirs.isEmpty()) {
             File current = dirs.poll();
             File[] nestedFiles = current.listFiles();
-            if (nestedFiles != null) {
-                for (File nestedFile : nestedFiles)
-                    if (nestedFile.isDirectory())
-                        dirs.offer(nestedFile);
+            for (File nestedFile : nestedFiles) {
+                if (nestedFile.isDirectory()) {
+                    dirs.offer(nestedFile);
+                }
             }
             count++;
             System.out.println(current.getName());
         }
-        System.out.println(count + " folders");
+        System.out.printf("%d folders%n", count);
     }
 }
